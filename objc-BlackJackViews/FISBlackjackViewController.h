f1cf5e14cbd090af2a5655fcbfb73169494c2844
John Richardson <richardsonjm@gmail.com>
2016-05-23 22:15:22 -0400

    Connect buttons and labels

---
+++ objc-BlackJackViews/FISBlackjackViewController.h
@@ -0,0 +1,39 @@
+//
+//  FISBlackjackViewController.h
+//  objc-BlackJackViews
+//
+//  Created by John Richardson on 5/23/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISBlackjackViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *winner;
+@property (weak, nonatomic) IBOutlet UILabel *house;
+@property (weak, nonatomic) IBOutlet UILabel *houseScore;
+@property (weak, nonatomic) IBOutlet UILabel *houseStayed;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard1;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard2;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard3;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard4;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard5;
+@property (weak, nonatomic) IBOutlet UILabel *houseBust;
+@property (weak, nonatomic) IBOutlet UILabel *houseBlackjack;
+@property (weak, nonatomic) IBOutlet UILabel *houseWins;
+@property (weak, nonatomic) IBOutlet UILabel *houseLosses;
+@property (weak, nonatomic) IBOutlet UILabel *player;
+@property (weak, nonatomic) IBOutlet UILabel *playerScore;
+@property (weak, nonatomic) IBOutlet UILabel *playerStayed;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard1;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard2;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard3;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard4;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard5;
+@property (weak, nonatomic) IBOutlet UILabel *playerBust;
+@property (weak, nonatomic) IBOutlet UILabel *playerBlackjack;
+@property (weak, nonatomic) IBOutlet UILabel *playerWins;
+@property (weak, nonatomic) IBOutlet UILabel *playerLosses;
+
+@end
---
+++ objc-BlackJackViews/FISBlackjackViewController.m
@@ -0,0 +1,49 @@
+//
+//  FISBlackjackViewController.m
+//  objc-BlackJackViews
+//
+//  Created by John Richardson on 5/23/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import "FISBlackjackViewController.h"
+
+@interface FISBlackjackViewController ()
+
+- (IBAction)deal:(id)sender;
+- (IBAction)hit:(id)sender;
+- (IBAction)stay:(id)sender;
+
+@end
+
+@implementation FISBlackjackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)hit:(id)sender {
+}
+
+- (IBAction)deal:(id)sender {
+}
+
+- (IBAction)stay:(id)sender {
+}
+@end
