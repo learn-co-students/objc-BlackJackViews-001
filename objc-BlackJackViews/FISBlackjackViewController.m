9509444d0b6cb36318855380173f55b63e8f4061
John Richardson <richardsonjm@gmail.com>
2016-05-24 11:40:08 -0400

    Set initial state of view

--- objc-BlackJackViews/FISBlackjackViewController.m
+++ objc-BlackJackViews/FISBlackjackViewController.m
@@ -21,6 +21,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.winner setHidden:YES];
+    [self.houseScore setHidden:YES];
+    [self.houseStayed setHidden:YES];
+    [self.houseCard1 setHidden:YES];
+    [self.houseCard2 setHidden:YES];
+    [self.houseCard3 setHidden:YES];
+    [self.houseCard4 setHidden:YES];
+    [self.houseCard5 setHidden:YES];
+    [self.houseBust setHidden:YES];
+    [self.houseBlackjack setHidden:YES];
+    self.playerScore.text = @"Score: 0";
+    [self.playerStayed setHidden:YES];
+    [self.playerCard1 setHidden:YES];
+    [self.playerCard2 setHidden:YES];
+    [self.playerCard3 setHidden:YES];
+    [self.playerCard4 setHidden:YES];
+    [self.playerCard5 setHidden:YES];
+    [self.playerBust setHidden:YES];
+    [self.playerBlackjack setHidden:YES];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,6 +61,7 @@
 }
 
 - (IBAction)deal:(id)sender {
+    [self.game dealNewRound];
 }
 
 - (IBAction)stay:(id)sender {
